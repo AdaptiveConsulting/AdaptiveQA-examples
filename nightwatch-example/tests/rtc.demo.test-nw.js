31ee694967003ce7728d495c83d85d878197f5f8
amilanan <anton.milanov@gmail.com>
2018-03-29 11:04:04 -0400

    Changed mocha to nightwatch runner, also now using the RTC demo site.

---
+++ nightwatch-example/tests/rtc.demo.test-nw.js
@@ -0,0 +1,34 @@
+const { getConfig } = require('../config');
+module.exports = {
+  '@tags': ['demo', 'example', 'web'],
+
+  before: function(browser) {
+    browser
+      .url(getConfig().host)
+      .waitForElementPresent('body', 5000)
+      .pause(5000);
+  },
+  after: function(browser) {
+    browser.end();
+  },
+
+  'Load RTC page and check the title : ': function(browser) {
+    browser.pause(5000).assert.title('Reactive Trader Cloud');
+  },
+  'Check the number of tiles displayed is 9': function(browser) {
+    browser
+      .waitForElementPresent('.workspace-region__item', 5000)
+      .elements('css selector', '.workspace-region__item', function(result) {
+        browser.assert.equal(result.value.length, 9);
+      });
+  },
+  'Check the Profit & Loss tile is displayed': function(browser) {
+    browser
+      .waitForElementPresent('.analytics__header-title', 5000)
+      .expect.element('.analytics__header-title')
+      .text.to.equal('Profit & Loss');
+    browser.expect
+      .element('.analytics__chart-title')
+      .text.to.equal('Positions');
+  }
+};
